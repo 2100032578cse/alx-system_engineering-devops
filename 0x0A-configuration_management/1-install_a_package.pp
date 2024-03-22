878535c07b105730a38102a99b844c2d662f1421
Sandrine <99196826+2100032578cse@users.noreply.github.com>
2024-03-22 23:34:03 +0530

    conf flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,18 @@
 #ensuring pip
 
-package {'python3-pip':
-ensure => 'present',
+package { 'python3-pip':
+  ensure => present,
 }
-
+# ensure that python present 
+package { 'python3.8':
+  ensure => present,
+}
+# Install Werkzeug 
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
 #install flask
 package {'flask':
 ensure   => '2.1.0',
