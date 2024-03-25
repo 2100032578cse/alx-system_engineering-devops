9c702acb7ddc6bf909fd720c22da778473372209
2100032578cse <uwizeyesandri@gmail.com>
2024-03-24 21:11:09 -0700

    task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,10 @@
-#connect the server
-file_line { 'no password':
+#file so that you can connect to a server without typing a password.
+file_line { 'no password connect':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
 }
-file_line { 'change private key':
+file_line { 'change the private key':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/school'
