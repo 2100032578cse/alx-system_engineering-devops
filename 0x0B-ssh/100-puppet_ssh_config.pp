238c706ad49011bbf4bb77877c44b8d2373913fd
Sandrine <99196826+2100032578cse@users.noreply.github.com>
2024-03-25 21:41:49 +0530

    task4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 #file so that you can connect to a server without typing a password.
 file { '/etc/ssh/ssh_config':
-	ensure 	=> present,
-content =>"
-
-	#client configuration
-	host*
-	IdentityFile ~/.ssh/school
-	PasswordAuthentication no"
+  ensure  => present,
+content => "
+    # ssh client configurations
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
 }
