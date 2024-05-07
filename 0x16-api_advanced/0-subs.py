5e82f5bda07202feddd25e222ac69c327011f329
Sandrine <99196826+2100032578cse@users.noreply.github.com>
2024-05-07 23:57:40 +0530

    task0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,9 +5,9 @@
  """
 import requests
 
-
+"""
 def number_of_subscribers(subreddit):
-    """the total number of subscribers on a given subreddi"""
+    
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
@@ -16,3 +16,18 @@ def number_of_subscribers(subreddit):
         return res
     else:
         return 0
+"""
+
+
+def number_of_subscribers(subreddit):
+    """the total number of subscribers on a given subreddi"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data["data"]["subscribers"]
+        return subscribers
+    else:
+        return 0
+     
