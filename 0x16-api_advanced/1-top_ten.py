b41007e1b65e1e1fed9f0b4b4d55fff79c3d631f
Sandrine <99196826+2100032578cse@users.noreply.github.com>
2024-05-08 00:25:32 +0530

    task1 fixing

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,24 @@
-i
+#!/usr/bin/python3
+"""
+    this module contains the function top_ten
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Function that queries the Reddit API if valid, else print None.
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"limit": 10},
+    )
+
+    if req.status_code == 200:
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            print(title)
+    else:
+        print(None)
