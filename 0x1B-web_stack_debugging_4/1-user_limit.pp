cb7278f0e190e3cbf701794de281ee02813a3bb8
Sandrine <uwizeyesandri@gmail.com>
2024-05-16 07:56:41 -0700

    added task 1 for os security config

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,5 @@
+# the OS security configuration
+exec {'config OS security':
+command => 'sed -i "s//holberton/foo/" /etc/security/limits.conf''
+path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
